1e28a25db7e54512983df926a4471b1aef6402fb
routeplanner <routeplanner@163.com>
2013-02-07 11:22:08 +0800

    Left Pannel changed to jquery ui

--- AntRoutePlanner/AntRoutePlanner/Scripts/js/Index.js
+++ AntRoutePlanner/AntRoutePlanner/Scripts/js/Index.js
@@ -1,13 +1,4 @@
-﻿    function plannerq(num) {
-        for (var id = 1; id <= 4; id++) {
-            var MrJin = "planner_con" + id;
-            if (id == num)
-                document.getElementById(MrJin).style.display = "block";
-            else
-                document.getElementById(MrJin).style.display = "none";
-        }
-        if (num == 1)
-            document.getElementById("plannertitle").className = "planner_title planner_title_bg1";
-        if (num == 2)
-            document.getElementById("plannertitle").className = "planner_title planner_title_bg2";
-    }
+﻿$(function () {
+    $("#planner").tabs();
+});
+
